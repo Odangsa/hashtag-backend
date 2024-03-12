43b0500107e44857e578025cc5f26d79676092d3
doghoney <andrew1044@naver.com>
2024-03-12 19:46:35 +0900

    [feat] WA-10 multipart request : 입출력 맞추기

--- hashtag-spring/src/main/java/com/odangsa/hashtag/controller/HashtagController.java
+++ hashtag-spring/src/main/java/com/odangsa/hashtag/controller/HashtagController.java
@@ -1,7 +1,8 @@
-package com.odangsa.hashtag.service;
+package com.odangsa.hashtag.controller;
 
 import com.odangsa.hashtag.dto.ResponseDTO;
 import com.odangsa.hashtag.dto.SectionDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("hashtag")
 public class HashtagController {
@@ -19,12 +22,24 @@ public class HashtagController {
             @RequestParam MultipartFile file, @RequestParam String place){
         String filename = "";
         if(!file.isEmpty()){
-            String filename = file.getOriginalFilename();
+            filename = file.getOriginalFilename();
         }
 
-        SectionDTO a = SectionDTO.builder().title(filename).hashtags().build();
-        SectionDTO b =
-        List<SectionDTO> dtos = null;
+        List<String> list = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
+        for(int i=0; i<10; i++){
+            list.add(filename);
+        }
+        SectionDTO a = SectionDTO.builder().title(filename).hashtags(list).build();
+
+        for(int i=0; i<10; i++){
+            list2.add(place);
+        }
+        SectionDTO b = SectionDTO.builder().title(place).hashtags(list2).build();
+        List<SectionDTO> dtos = new ArrayList<>();
+        dtos.add(a);
+        dtos.add(b);
+
         ResponseDTO<SectionDTO> response = ResponseDTO.<SectionDTO>builder().data(dtos).build();
         return ResponseEntity.ok().body(response);
     }
