ea8336ca231330fdaf6cc2269c859e030f4534c0
doghoney <andrew1044@naver.com>
2024-05-25 17:03:06 +0900

    [fix] WA-22 https 설정

--- hashtag-spring/src/main/java/com/odangsa/hashtag/HashtagApplication.java
+++ hashtag-spring/src/main/java/com/odangsa/hashtag/HashtagApplication.java
@@ -1,8 +1,12 @@
 package com.odangsa.hashtag;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
